7375172fa0918993d39fd97fa05bd028e1ccc89f
eduardopedroza <ea.pedroza23@gmail.com>
2024-01-14 17:02:30 +0100

    added login and register routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,39 @@
+const express = require("express");
+const router = new express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+
+const ExpressError = require("../expressError");
+const db = require("../db");
+const { SECRET_KEY, BCRYPT_WORK_FACTOR } = require("../config");
+const User = require('../models/user')
+
+
+
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
 
+router.post('/login', async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.get(username);
+
+    if (user && await bcrypt.compare(password, user.password)) {
+      let token = jwt.sign({ username }, SECRET_KEY);
+      await User.updateLoginTimestamp(username);
+      return res.json({ token });
+    }
+
+    throw new ExpressError("Invalid user/password", 400);
+  } catch (e) {
+    return next(e);
+  }
+});
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
@@ -11,3 +41,19 @@
  *
  *  Make sure to update their last-login!
  */
+
+router.post('/register', async (req, res, next) => {
+  try {
+    const { username, password, first_name, last_name, phone } = req.body;
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    const user = await User.register({ username, hashedPassword, first_name, last_name, phone });
+
+    let token = jwt.sign({ username }, SECRET_KEY);
+    await User.updateLoginTimestamp(username);
+    return res.json({ token });
+  } catch (e) {
+    return next(e);
+  }
+});
+
+module.exports = router;
